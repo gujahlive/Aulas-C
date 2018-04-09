4bd7230206292445f544be43b7c7134cafca6fcf
Gustavo Ferreira <gustavofvieira@live.com>
2018-04-09 11:10:56 -0300

    subinuu

--- Realloc/1.c
+++ Realloc/1.c
@@ -28,40 +28,70 @@ typedef struct{
 	printf("1 - Cadastrar Animal\n");
 	printf("1 - Cadastrar Animal\n");
 }*/
-main(){
-	Tanimal *animal;
-	int *n,n1,i,novosAnimais,result;
-	char sorn;
-	n = &n1;
-	printf("\t\t\t\tCadastro Animal\n");
-	printf("Quantos Animal Deseja Cadastrar? : ");
-	scanf("%d",&n1);
-	animal = (Tanimal *) malloc (*n * sizeof(Tanimal));
-	for (i = 0 ; i < n ; i++){
+Tanimal ExibeAnimal(int quant){
+
+Tanimal *animal;
+int i;
+	printf("\t\t\t\tAnimais Cadastrados\n");
+	for (i = 0 ; i < quant ; i++){
+		printf("\t\t\t\tAnimal %d\n",i+1);
+		printf("Nome : %s\n",animal[i].nome);
+		printf("Idade: %d\n",animal[i].idade);
+		printf("Especie: %s\n",animal[i].especie);
+		
+	}
+
+
+}
+
+
+
+
+Tanimal AlocaAnimal(int quant){	
+Tanimal *animal;
+int i;
+animal = (Tanimal *) malloc (quant * sizeof(Tanimal));
+for (i = 0 ; i < quant ; i++){
 		fflush(stdin);
 		printf("\t\t\t\tCadastro Animal\n");
-		printf("Nome : ");
-		gets((*animal).nome);
+		printf("Nome : ");	
+		gets((animal+i)->nome);// como vou colocar o indice AQUI??
 		fflush(stdin);
 		printf("Idade: ");
-		scanf("%d",&(*animal).idade);
+		scanf("%d",&(animal+i)->idade);
+		//scanf("%d",&animal[i].idade);
 		fflush(stdin);
 		printf("Especie: ");
-		gets((*animal).especie);
+		gets((animal+i)->especie);
+	//	gets(animal[i].especie);
 		fflush(stdin);
-		
+//		animal++;
 		//printf("\t\t\t\t\tAnimal Cadastrado\nNome: %s\nIdade: %d\nEspecie: %s\n",(*animal).nome,(*animal).idade,(*animal).especie);
 		//AlocaAnimal(&animal);
 		
 	}
-	printf("\t\t\t\tAnimais Cadastrados\n");
-	for (i = 0 ; i < n ; i++){
+	
+	for (i = 0 ; i < quant ; i++){
 		printf("\t\t\t\tAnimal %d\n",i+1);
-		printf("Nome : %s\n",(animal)[i].nome);
-		printf("Idade: %d\n",(animal)[i].idade);
-		printf("Especie: %s\n",(animal)[i].especie);
-		
-	}
+		printf("Nome : %s\n",animal[i].nome);
+		printf("Idade: %d\n",animal[i].idade);
+		printf("Especie: %s\n",animal[i].especie);
+		}
+
+
+
+
+}
+main(){
+	Tanimal *animal;
+	int n,n1,i,novosAnimais,result;
+	char sorn;
+	printf("\t\t\t\tCadastro Animal\n");
+	printf("Quantos Animal Deseja Cadastrar? : ");
+	scanf("%d",&n);
+	AlocaAnimal(n);
+	
+	
 	printf("deseja cadastrar mais? S=SIM ou N=NAO: ");	
 	scanf("%c",&sorn);
 	if (sorn == 's'){
@@ -75,12 +105,7 @@ main(){
 		
 	} else 
 	if (sorn == 'n'){	
-		for (i = 0 ; i < n ; i++){
-			printf("\t\t\t\tAnimal %d\n",i+1);
-			printf("Nome : %s\n",(animal)[i].nome);
-			printf("Idade: %d\n",(animal)[i].idade);
-			printf("Especie: %s\n",(animal)[i].especie);		
-		}
+		ExibeAnimal(n);
 		
 	}
 
