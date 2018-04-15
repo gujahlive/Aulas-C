d228a65379b3ee7d3163bff785416e6aec95bf6c
Gustavo Ferreira <gustavofvieira@live.com>
2018-04-15 18:23:04 -0300

    lista

--- Lista de Atividades/Lista PA 2018.1/Resolução/70.c
+++ Lista de Atividades/Lista PA 2018.1/Resolução/70.c
@@ -6,5 +6,28 @@ Alocação Dinâmica
 #include<stdio.h>
 #include<stdlib.h>
 main(){
+	int i,n,m,*pn,*pm;
+	printf("Insira N: ");
+	scanf("%d",&n);
+	printf("Insira M: ");
+	scanf("%d",&m);	
+	pn = (int *) malloc (n * sizeof(int));
+	pm = (int *) malloc (m * sizeof(int));
+	for(i=0;i<n;i++){
+			printf("N[%d] Representando LINHA: ",i);
+			scanf("%d",&pn[i]);	
+	}
+	for(i=0;i<m;i++){
+			printf("M[%d] Representando COLUNA: ",i);
+			scanf("%d",&pm[i]);	
+	}
+
+	for (i=0;i<n;i++){
+		printf("%d n \n",pn[i]);
+		for(i=0;i<m;i++){
+			printf("%d  m \t",pm[i]);	
+		}
+		
+	}
 	
 }
